7707a36fa9d4bc51b3a91bd9806919420dda09ec
Tristan Neumann <tristan.neumann@hotmail.com>
2018-07-31 19:53:20 -0700

    made readMe;

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # productDescription
+
+npm i
+
+Seed Data:
+  Create Db: product in postgres
+  create config with db username and pass
+
+npm start
+npm run seed
+npm run build
+
--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
+const {dbUsername, dbPassword} = require('../config.js');
 
-const product = new Sequelize('product', 'postgres', 'postgres1', {
+const product = new Sequelize('product', dbUsername, dbPassword, {
   host: 'localhost',
   dialect: 'postgres',
   operatorsAliases: false,
